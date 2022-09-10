986f38e6756a7ef9fa84ba352e9a97fde5491916
AJEETKR123 <103138329+AJEETKR123@users.noreply.github.com>
2022-09-10 14:28:21 +0530

    added:salary

--- src/com/bridzelabz/UC1.java
+++ src/com/bridzelabz/UC1.java
@@ -2,11 +2,29 @@ package com.bridzelabz;
 
 public class UC1 {
     public static void main(String[] args) {
+
         int Is_Full_Time = 1;
+
+        int Emp_Rate_Per_Hrs = 20;
+
+        int Emp_hrs = 0;
+
+        int Emp_wage=0;
+
         double empCheck = Math.floor(Math.random() * 10)%2;
+
         if (empCheck == Is_Full_Time)
+        {
             System.out.println("Employee is present ");
-        else
+            Emp_hrs = 8;
+            Emp_wage = Emp_Rate_Per_Hrs * Emp_hrs;
+            System.out.println("Employe wage salary is " + Emp_wage);
+        }
+        else {
             System.out.println("Employee is absent ");
+            Emp_hrs = 0;
+            Emp_wage = Emp_Rate_Per_Hrs * Emp_hrs;
+            System.out.println(Emp_wage);
+        }
     }
 }
