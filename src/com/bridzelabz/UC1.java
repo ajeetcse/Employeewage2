a6e5ac0915b40d7cb28ba0a00a0497419eec92ff
AJEETKR123 <103138329+AJEETKR123@users.noreply.github.com>
2022-09-10 14:18:22 +0530

    added:attendence

---
+++ src/com/bridzelabz/UC1.java
@@ -0,0 +1,12 @@
+package com.bridzelabz;
+
+public class UC1 {
+    public static void main(String[] args) {
+        int Is_Full_Time = 1;
+        double empCheck = Math.floor(Math.random() * 10)%2;
+        if (empCheck == Is_Full_Time)
+            System.out.println("Employee is present ");
+        else
+            System.out.println("Employee is absent ");
+    }
+}
