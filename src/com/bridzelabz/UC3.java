704a51035891abf1c63a44af3a06bff682fa51bb
AJEETKR123 <103138329+AJEETKR123@users.noreply.github.com>
2022-09-10 14:53:08 +0530

    added:switch

--- src/com/bridzelabz/UC3.java
+++ src/com/bridzelabz/UC3.java
@@ -3,8 +3,8 @@ package com.bridzelabz;
 public class UC3 {
     public static void main(String[] args) {
 
-        int Is_Full_Time = 1;
-        int Is_Part_Time =2;
+        final int  Is_Full_Time = 1;
+        final int Is_Part_Time =2;
 
         int Emp_Rate_Per_Hrs = 20;
 
@@ -12,20 +12,21 @@ public class UC3 {
 
         int Emp_wage=0;
 
-        double empCheck = Math.floor(Math.random() * 10)%2;
-
-        if (empCheck == Is_Full_Time)
-        {
+        int  empCheck =(int) Math.floor(Math.random() * 10)%2;
+        switch (empCheck){
+            case Is_Full_Time:
             System.out.println("Employee is Full time ");
             Emp_hrs = 8;
             Emp_wage = Emp_Rate_Per_Hrs * Emp_hrs;
             System.out.println("Employe wage salary is " + Emp_wage);
-        } else if (empCheck == Is_Part_Time) {
+            break;
+            case Is_Part_Time:
             System.out.println("Employe is part time");
             Emp_hrs =4;
             Emp_wage =Emp_Rate_Per_Hrs * Emp_hrs;
             System.out.println(Emp_wage);
-        } else {
+            break;
+            default:
             System.out.println("Employee is absent ");
             Emp_hrs = 0;
             Emp_wage = Emp_Rate_Per_Hrs * Emp_hrs;
